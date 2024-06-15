332657549c908813776c1f2c0b28ca48e185b04e
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-15 11:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function firstChar(text) {
   // your code here
-	return text[0]
+	 // Trim the input text to remove leading and trailing spaces
+  text = text.trim();
+  
+	 if (text === '') {
+    return ''; // Return empty string if all spaces
+  }
+  
+  // Return the first character of the trimmed text
+  return text[0];
 }
 
 // Do not change the code below
